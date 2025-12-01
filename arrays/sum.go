a6dd490f86419a57aba1e53fcdc1b8c34719f655
Pradist Keawborisut <pradist.keawborisut@krungsri.com>
2025-12-01 22:04:05 +0700

    test: improve test case naming with Given-When-Then pattern

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,5 +1,9 @@
 package arrays
 
 func Sum(numbers [5]int) int {
-	return 15
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -4,12 +4,21 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	numbers := [5]int{1, 2, 3, 4, 5}
-
-	got := Sum(numbers)
-	want := 15
-
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	testCases := []struct {
+		numbers  [5]int
+		expected int
+	}{
+		{[5]int{1, 2, 3, 4, 5}, 15},
+		{[5]int{5, 5, 5, 5, 5}, 25},
 	}
+
+	for _, testCase := range testCases {
+		t.Run("given array of five numbers when sum then returns correct total", func(t *testing.T) {
+			got := Sum(testCase.numbers)
+			if got != testCase.expected {
+				t.Errorf("got %d want %d given, %v", got, testCase.expected, testCase.numbers)
+			}
+		})
+	}
+
 }
